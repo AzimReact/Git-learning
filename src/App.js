ee2ea1077a52931a2b2b7d1889735ae0d818c091
Ainarka <ainarka11@gmail.com>
2024-09-12 20:50:41 +0600

    add section1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header';
+import Section1 from './components/Section1';
 import './App.css';
 
 function App() {
   return (
     <div>
       <Header />
+      <Section1/>
     </div>
   );
 }
